3bc457f46805ba38f34a12d4ecc360000bfca7e7
Kishan <66522922+k-shar@users.noreply.github.com>
2020-06-15 20:19:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var startTime = new Date().getTime();
 var move_hex = function() {
 	var currTime = new Date().getTime();
 	var x = currTime-startTime
-	var newHeight = (70 + (-1* Math.sqrt(x)))
+	var newHeight = (100 + (-1.5* Math.sqrt(x)))
 	hexagon.style.marginTop = newHeight + "vh";
 	
 	if (newHeight > 12) {
