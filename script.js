ae14909118b6ba8bc607d6a41628371c252852fc
Kishan <66522922+k-shar@users.noreply.github.com>
2020-06-16 18:26:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-var hexagon = document.getElementById("hexagon");
-
-var startTime = new Date().getTime();
-var move_hex = function() {
-	var currTime = new Date().getTime();
-	var x = currTime-startTime
-	var newHeight = (100 + (-1.5* Math.sqrt(x)))
-	hexagon.style.marginTop = newHeight + "vh";
-	
-	if (newHeight > 12) {
-		window.requestAnimationFrame(move_hex);
-	}
-}
-
-move_hex();
-
 function refreshPage(){
     window.location.reload();
 } 
+
+$(document).ready(function(){
+	$("#hexagon").animate({marginTop: "5vh"}, 4000, 'swing');
+});
