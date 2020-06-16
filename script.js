1ebfb2fbce12b4b082ca1c9564129ef03920a28c
Kishan <66522922+k-shar@users.noreply.github.com>
2020-06-16 21:03:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,41 +6,58 @@ $(document).ready(function(){
 	$("#hexagon").animate({marginTop: "5vh"}, 4000, 'swing');
 });
 
-var one_to_five = 1;
-var call_time = 1;
+var call_time = 0;
 
 var startTime = new Date().getTime();
+
 var node_one = document.getElementById("trap_l");
 var node_two = document.getElementById("trap_r");
-
 var node_three = document.getElementById("trap_bl");
 var node_four = document.getElementById("trap_br");
 
+var one_start = 15;
+var two_start = -15;
+var three_start = 25;
+var four_start = -25;
+
+var a = 1;
+var b = -1;
+
 var spin = function () {
-	if (call_time < 0) {
+	if (call_time === 0) {
 		startTime = new Date().getTime();
 		call_time = 1;
-	}
-	if (one_to_five === 1) {
-		var currTime = new Date().getTime();
-		var seconds = (currTime - startTime)/ 1000;
-		
-		new_rotation_l = (15 - seconds * 7 * Math.sqrt(seconds));
-		new_rotation_r = (-15 - seconds * -7 * Math.sqrt(seconds));
-		new_rotation_bl = (25 - seconds * 6 * Math.sqrt(seconds));
-		new_rotation_br = (-25 - seconds * -6 * Math.sqrt(seconds));
-		
-		node_one.style.transform = "skew("+new_rotation_l+"deg)";
-		node_two.style.transform = "skew("+new_rotation_r+"deg)";
-		node_three.style.transform = "skew("+new_rotation_bl+"deg)";
-		node_four.style.transform = "skew("+new_rotation_br+"deg)";
-		
-		if (seconds < 10) {
-			window.requestAnimationFrame(spin);
+		if (Math.round(Math.random()) === 0 )
+		{
+			a = 1;
+			b = -1;
 		} else {
-			in_or_out = 1;
-			call_time = 0;
+			a = -1;
+			b = 1;
 		}
-
 	}
+	
+	var currTime = new Date().getTime();
+	var seconds = (currTime - startTime)/ 1000;
+	
+	
+	new_rotation_l = (one_start - seconds * a* 17 * Math.sqrt(seconds));
+	new_rotation_r = (two_start - seconds * b* 17 * Math.sqrt(seconds));
+	new_rotation_bl = (three_start - seconds * a* 16 * Math.sqrt(seconds));
+	new_rotation_br = (four_start - seconds * b* 16 * Math.sqrt(seconds));
+	
+	node_one.style.transform = "skew("+new_rotation_l+"deg)";
+	node_two.style.transform = "skew("+new_rotation_r+"deg)";
+	node_three.style.transform = "skew("+new_rotation_bl+"deg)";
+	node_four.style.transform = "skew("+new_rotation_br+"deg)";
+	
+	if (seconds < 4) {
+		window.requestAnimationFrame(spin);
+	} else {
+		call_time = 0;
+		one_start = new_rotation_l;
+		two_start = new_rotation_r;
+		three_start = new_rotation_bl;
+		four_start = new_rotation_br;
+	}	
 }
