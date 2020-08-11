9af1880485940fe801c3c8c6dc117aa57fe6e865
Kishan <66522922+k-shar@users.noreply.github.com>
2020-08-11 16:33:51 +0100

    Add files via upload

---
+++ links/links.js
@@ -0,0 +1,19 @@
+var x = document.getElementsByClassName("link_one");
+var i;
+for (i = 0; i < x.length; i++) {
+	x[i].href = "../quiz/quiz.html";
+}
+
+var x = document.getElementsByClassName("link_two");
+var i;
+for (i = 0; i < x.length; i++) {
+	x[i].href = "../index.html";
+}
+
+var x = document.getElementsByClassName("link_three");
+var i;
+for (i = 0; i < x.length; i++) {
+	x[i].href = "../about/about.html";
+}
+
+
