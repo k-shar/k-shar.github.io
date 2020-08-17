40fcabdb097b18347d2be359dea234c4c8c39a05
Kishan <66522922+k-shar@users.noreply.github.com>
2020-08-17 20:43:53 +0100

    Add files via upload

---
+++ quiz_linker.js
@@ -0,0 +1,14 @@
+/*
+var x = document.getElementById("quiz_one");
+x.href = "quizes/quiz_template.html";
+*/
+
+var x = document.getElementById("quiz_two");
+x.href = "quizes/furry_quiz.html";
+
+/*
+var x = document.getElementById("quiz_three");
+x.href = "https://github.com/k-shar/k-shar.github.io";
+*/
+var x = document.getElementById("back");
+x.href = "../links/links.html"
